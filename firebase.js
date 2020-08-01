9078978960de1c65eded796c4913fd3c20c13f00
sathyanarayan <sathyanmbpk@gmail.com>
2020-08-01 20:28:01 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var app_firebase = {};
+(function(){
+  var config = {
+    apiKey: "AIzaSyBjIcZwXZsy96OMT_WJCZQlOIt3grF_QzU",
+    authDomain: "focess-bdea9.firebaseapp.com",
+    databaseURL: "https://focess-bdea9.firebaseio.com",
+    projectId: "focess-bdea9",
+    storageBucket: "focess-bdea9.appspot.com",
+    messagingSenderId: "993638462166",
+    appId: "1:993638462166:web:a7c6b42ddacbd90dd4cf77",
+    measurementId: "G-HDM4Q9P4LZ"
+  };
+  firebase.initializeApp(config);
+  app_firebase = firebase;
+
+})()
---
+++ login.js
@@ -0,0 +1,72 @@
+(function () {
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
+                return true;
+            },
+            uiShown: function () {
+                // The widget is rendered.
+                // Hide the loader.
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: 'main.html',
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            {
+                provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                scopes: [
+                    'https://www.googleapis.com/auth/contacts.readonly'
+                ],
+                customParameters: {
+                    // Forces account selection even when one account
+                    // is available.
+                    prompt: 'select_account'
+                }
+            },
+            {
+                provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                scopes: [
+                    'public_profile',
+                    'email',
+                    'user_likes',
+                    'user_friends'
+                ],
+                customParameters: {
+                    // Forces password re-entry.
+                    auth_type: 'reauthenticate'
+                }
+            },
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            {
+            provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            recaptchaParameters: {
+                type: 'image',
+                size: 'normal',
+                badge: 'bottomleft'
+            },
+            defaultCountry: 'GB',
+            defaultNationalNumber: '1234567890',
+            loginHint: '+11234567890'
+    }
+        ],
+    // Terms of service url.
+    tosUrl: 'terms.html',
+        // Privacy policy url.
+        privacyPolicyUrl: 'privacy.html'
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+    
+    
+
+}) ()
